519a789c8ce84cab6261669ca2f7ea3a64762b6c
THARUNSAI REDDY C <46275022+THARUNSAIREDDY@users.noreply.github.com>
2021-05-04 11:48:05 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -84,7 +84,10 @@ DATABASES = {
     }
 }
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
